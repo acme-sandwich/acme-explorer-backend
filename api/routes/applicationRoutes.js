63df928664a46823799d9a6ebd69bb2a7f9bc26e
raffrearaUS <raffrearaus@gmail.com>
2020-02-23 16:52:28 +0100

    Fix: updated routes

--- api/routes/applicationRoutes.js
+++ api/routes/applicationRoutes.js
@@ -2,12 +2,15 @@
 module.exports = function(app) {
   var applications = require('../controllers/applicationController');
 
-  app.route('/applications')
+  app.route('/v1/actors/:actorId/applications')
 	  .get(applications.list_all_applications)
 	  .post(applications.create_an_application);
 
-  app.route('/applications/:applicationId')
+  app.route('/v1/actors/:actorId/applications/:applicationId')
     .get(applications.read_an_application)
 	  .put(applications.update_an_application)
     .delete(applications.delete_an_application);
+  
+  app.route('/v1/trips/:tripId/applications')
+    .get(applications.list_all_applications);
 };
