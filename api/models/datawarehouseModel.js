99012d55e7e78b2cdc8688beadab41077f6120b6
Antonio Rodríguez Artacho <antonio.rdgz.a@gmail.com>
2020-02-25 18:25:21 +0100

    Add: datawarehouse model

--- api/models/datawarehouseModel.js
+++ api/models/datawarehouseModel.js
@@ -3,5 +3,37 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DataWareHouseSchema = new mongoose.Schema({
-    
-})
+    tripsPerManagers: [{
+        type: Number
+    }],
+    applicationsPerTrip: [{
+        type: Number
+    }],
+    pricePerTrip: [{
+        type: Number
+    }],
+    ratioApplicationsPerStatus: [{
+        type: Number,
+        min: 0,
+        max: 1
+    }],
+    avgFinderPrices: {
+        type: Number,
+        min: 0
+    },
+    topFinderWords: [{
+        word: String,
+        wordSum: Number
+    }],
+    computationMoment: {
+        type: Date,
+        default: Date.now
+    },
+    rebuildPeriod: {
+        type: String
+    }
+}, {strict: false});
+
+DataWareHouseSchema.index({computationMoment: -1});
+
+module.exports = mongoose.model('DataWareHouse', DataWareHouseSchema);
